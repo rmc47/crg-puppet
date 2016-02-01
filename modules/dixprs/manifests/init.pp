4b722915468756dc755fff26801d2a2f0cc2647f
Robert Chipperfield <robert@syxis.co.uk>
2016-02-01 23:25:54 +0000

    Template for config.txt

--- modules/dixprs/manifests/init.pp
+++ modules/dixprs/manifests/init.pp
@@ -1,5 +1,10 @@
 class dixprs {
 	require dixprs::install
 	require dixprs::instance
-	
+	require dixprs::params
+
+	file { '/home/pi/dixprs/config.txt':
+		ensure => file,
+		content => template('dixprsconfig.txt.erb'),
+	}
 }
