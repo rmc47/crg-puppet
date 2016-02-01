2cac0954b8d29dae4ecc7f9f6faee1188d4b69d6
Robert Chipperfield <robert@syxis.co.uk>
2016-02-01 23:44:14 +0000

    Correct puppet apt repo for latest raspbian releases

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-wget https://apt.puppetlabs.com/puppetlabs-release-wheezy.deb
+wget https://apt.puppetlabs.com/puppetlabs-release-jessie.deb
 sudo dpkg -i puppetlabs-release-wheezy.deb
 sudo apt-get update
 sudo apt-get install puppet
