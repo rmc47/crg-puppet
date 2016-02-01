198566f1b533964196ea9aeb65bb70218930eebd
Robert Chipperfield <robert@syxis.co.uk>
2016-02-01 23:44:33 +0000

    Hmm, still fiddling with best way of getting params in

--- modules/dixprs/manifests/init.pp
+++ modules/dixprs/manifests/init.pp
@@ -1,9 +1,9 @@
 class dixprs {
 	require dixprs::install
-	require dixprs::params
 
 	file { '/home/pi/dixprs/config.txt':
 		ensure => file,
-		content => template('dixprsconfig.txt.erb'),
+		content => template('dixprs/dixprsconfig.txt.erb'),
+		require => Class['dixprs::params'],
 	}
 }
