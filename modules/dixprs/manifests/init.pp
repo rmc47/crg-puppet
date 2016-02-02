67394296e84e95fa7fac7fb115123c33ea5fefa3
Robert Chipperfield <robert@syxis.co.uk>
2016-02-02 23:46:06 +0000

    Disable the serial console

--- modules/dixprs/manifests/init.pp
+++ modules/dixprs/manifests/init.pp
@@ -12,15 +12,25 @@ class dixprs (
 
 	require dixprs::install
 
+	# Make ourselves a nice config file from the template
 	file { '/home/pi/dixprs/config.txt':
 		ensure => file,
 		content => template('dixprs/dixprsconfig.txt.erb'),
 	}
 
+	# Start DIXPRS on boot
 	cron { 'dixprs-on-reboot':
 		ensure => present,
 		command => '/home/pi/dixprs/dixprs.py -c /home/pi/dixprs/config.txt',
 		user => 'pi',
 		special => 'reboot',
+	}
+
+	# Disable the RS232 console on the Pi, because it eats up the serial port
+	exec { 'disable-console-tty':
+		command => 'sed -i "s/console=ttyAMA0[^\\ ]*\\ //" /boot/cmdline.txt',
+		cwd => '/',
+		shell => true,
+		unless => 'grep -q ttyAMA /boot/cmdline.txt',
 	}
 }
