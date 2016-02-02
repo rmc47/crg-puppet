4ab538d24b8c661111001ac2a78ab7208e731881
Robert Chipperfield <robert@syxis.co.uk>
2016-02-02 22:39:56 +0000

    Hmm, another try at this variable passing lark

--- modules/dixprs/manifests/init.pp
+++ modules/dixprs/manifests/init.pp
@@ -1,7 +1,17 @@
-class dixprs {
-	require dixprs::install
-	require dixprs::params
+class dixprs (
+	$callsign, 
+	$longitude, 
+	$latitude, 
+	$symbol,
+	$beaconText,
+	$aprsisHost,
+	$aprsisFilter,
+	$kissPort,
+	$serialSpeed
+	) {
 	
+	require dixprs::install
+
 	file { '/home/pi/dixprs/config.txt':
 		ensure => file,
 		content => template('dixprs/dixprsconfig.txt.erb'),
--- modules/role_mb7ups_pi/manifests/init.pp
+++ modules/role_mb7ups_pi/manifests/init.pp
@@ -1,3 +1,3 @@
 class role_mb7ups_pi {
-	include dixprs
+	class {'dixprs':}
 }
