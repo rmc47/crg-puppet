70c46be97d12e79e63cb152d985da9d91b6c46c2
Robert Chipperfield <robert@syxis.co.uk>
2016-02-02 23:19:05 +0000

    Start dixprs on system startup via a cron job

--- modules/dixprs/manifests/init.pp
+++ modules/dixprs/manifests/init.pp
@@ -9,11 +9,18 @@ class dixprs (
 	$kissPort,
 	$serialSpeed
 	) {
-	
+
 	require dixprs::install
 
 	file { '/home/pi/dixprs/config.txt':
 		ensure => file,
 		content => template('dixprs/dixprsconfig.txt.erb'),
+	}
+
+	cron { 'dixprs-on-reboot':
+		ensure => present,
+		command => '/home/pi/dixprs/dixprs.py -c /home/pi/dixprs/config.txt',
+		user => 'pi',
+		special => 'reboot',
 	}
 }
